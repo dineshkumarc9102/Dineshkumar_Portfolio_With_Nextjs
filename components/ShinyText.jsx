5299ecbe144db9e33bc12eabac70b1410dbcf8f4
Dinesh Kumar <125630400+dineshkumarc9102@users.noreply.github.com>
2026-06-24 13:53:24 +0530

    Improve className formatting in ShinyText component
    Refactor className formatting for better readability.

--- components/ShinyText.jsx
+++ components/ShinyText.jsx
@@ -1,11 +1,12 @@
 
-const ShinyText = ({ text, disabled = false, speed = 5, className = '', isDarkMode}) => {
+const ShinyText = ({ text, disabled = false, speed = 5, className = '', isDarkMode }) => {
   const animationDuration = `${speed}s`;
 
   return (
     <div
-      className={`text-[#000000] dark:text-[#ffffffa4] text-3xl sm:text-xl md:text-5xl bg-clip-text inline-block font-Ovo ${!disabled && isDarkMode ? 'animate-shine' : ''} ${className}`}
-      style={{
+      className={`text-[#000000] dark:text-[#ffffffa4]
+      bg-clip-text inline-block font-Ovo
+      ${!disabled && isDarkMode ? 'animate-shine' : ''} ${className}`} style={{
         backgroundImage:
           'linear-gradient(120deg, rgba(255,255,255,0) 40%, rgba(255,255,255,0.8) 50%, rgba(255,255,255,0) 60%)',
         backgroundSize: '200% 100%',
