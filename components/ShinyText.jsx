3f4fa5ab8d32536440164ad4f47c7760f2d6b83e
Dinesh Kumar <125630400+dineshkumarc9102@users.noreply.github.com>
2026-08-29 16:02:06 +0530

    Change ShinyText from div to span element

--- components/ShinyText.jsx
+++ components/ShinyText.jsx
@@ -3,7 +3,7 @@ const ShinyText = ({ text, disabled = false, speed = 5, className = '', isDarkMo
   const animationDuration = `${speed}s`;
 
   return (
-    <div
+    <span
       className={`text-[#000000] dark:text-[#ffffffa4]
       bg-clip-text inline-block font-Ovo
       ${!disabled && isDarkMode ? 'animate-shine' : ''} ${className}`} style={{
@@ -15,7 +15,7 @@ const ShinyText = ({ text, disabled = false, speed = 5, className = '', isDarkMo
       }}
     >
       {text}
-    </div>
+    </span>
   );
 };
 
